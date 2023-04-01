a07c0ced91b95d8fbae722adebb1216eb15f701d
Erzhan004 <124499458+Erzhan004@users.noreply.github.com>
2023-04-01 16:12:02 +0600

    -

--- Rectangle/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-       Rectangle rectangle = new Rectangle(5,4 ,6,3);
-        System.out.println("Triangle perimeter with parameters " + rectangle.getA()  + ", " + rectangle.getB()+ ", " + rectangle.getC() + ", " + rectangle.getD() + " is " + rectangle.getPerimeter());
-    }
-}
